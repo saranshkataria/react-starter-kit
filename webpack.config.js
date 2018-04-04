6af8c9efdf5083af05485b6aff9884dab802db2c
saranshkataria <saransh@wisdomgeek.com>
2018-04-04 12:05:54 +0530

    added hot module reloading on server side

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devServer: {
+    hot: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
       hash: true,
       title: 'React Starter Kit by Saransh Kataria'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 };

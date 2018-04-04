f7d73560dcb4443f272c31fecce41861a3a8e50a
saranshkataria <saransh@wisdomgeek.com>
2018-04-04 14:22:11 +0530

    added babel loader and checks for prod vs dev build in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,30 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  devServer: {
-    hot: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true,
-      title: 'React Starter Kit by Saransh Kataria'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+module.exports = (env, argv) => {
+  let baseConfig = {};
+  if (argv.mode === 'development') {
+    baseConfig.devServer = {};
+    baseConfig.devServer.hot = true;
+    baseConfig.devServer.overlay = true;
+    baseConfig.plugins = {};
+    baseConfig.plugins.push(
+      new HtmlWebpackPlugin({
+        hash: true,
+        title: 'React Starter Kit by Saransh Kataria'
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    );
+  } else {
+    baseConfig.module = {
+      rules: [
+        {
+          test: /\.js/,
+          exclude: /node_modules/,
+          use: { loader: 'babel-loader' }
+        }
+      ]
+    };
+  }
+  return baseConfig;
 };
